af2f0ce9023075cfa9d2e0aa295c01eb6c7075dd
gyl <gyl33333@gmail.com>
2015-04-17 10:26:39 +0800

    [vim][gyl]Modify command ast to support a file not a dirtcory.
    Modify command ast to support a file not a dirtcory.

--- bin/source_file/ast.sh
+++ bin/source_file/ast.sh
@@ -1,10 +1,14 @@
 
 ast () {
 	value="$@"
-	astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.c
-	astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.h
-	astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cc
-	astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cpp
+	if [ -d $value ]; then
+		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.h
+		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.c
+		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cpp
+		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cc
+	else
+		astyle --style=linux -p --indent=force-tab --suffix=none $value
+	fi
 	value=""
 }
 
