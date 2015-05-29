8bd3f43852b62bfbe1d84014e15b98066eabcc43
gyl <gyl33333@gmail.com>
2015-05-29 11:58:31 +0800

    [vim][gyl]Modify the cmd ast.
    Modify the cmd ast to support the signal directory.

--- bin/source_file/ast.sh
+++ bin/source_file/ast.sh
@@ -2,10 +2,20 @@
 ast () {
 	value="$@"
 	if [ -d $value ]; then
-		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.h
-		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.c
-		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cpp
-		astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cc
+		if [ $(find $value -type d | wc -l) -gt 1 ]; then
+
+			echo "in >1"
+			astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cc
+			astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.cpp
+			astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.h
+			astyle --style=linux -p --indent=force-tab --suffix=none --recursive $value/*.c
+		else
+			astyle --style=linux -p --indent=force-tab --suffix=none $value/*.cc
+			astyle --style=linux -p --indent=force-tab --suffix=none $value/*.cpp
+			astyle --style=linux -p --indent=force-tab --suffix=none $value/*.h
+			astyle --style=linux -p --indent=force-tab --suffix=none $value/*.c
+
+		fi
 	else
 		astyle --style=linux -p --indent=force-tab --suffix=none $value
 	fi
