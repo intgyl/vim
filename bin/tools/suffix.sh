4f4417572b68da3cdcf785988d5e9c2d43f653e4
gyl <gyl33333@gmail.com>
2015-08-11 14:20:37 +0800

    [vim][gyl]Modify the command suffix
    Modify the command suffix

--- bin/tools/suffix.sh
+++ bin/tools/suffix.sh
@@ -1,4 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
 suffix() {
-	rename 's/\.$1/\.$2/' $3/*
+	if [ -z $1 ]; then
+		print_suffix_usage
+		return
+	fi
+
+	if [ -z $2 ]; then
+		print_suffix_usage
+		return
+	fi
+
+	if [ -z $3 ]; then
+		print_suffix_usage
+		return
+	fi
+
+	if [ ! -e $3 ]; then
+		echo 'No such file or directory'
+		return
+	fi
+
+	if [ -d $3 ]; then
+		cd $3
+		old_name=$1
+		new_name=$2
+
+		find ./ -name "*.${old_name}" | awk -F "." '{print $2}' | xargs -i -t mv ./{}.${old_name} ./{}.${new_name}
+
+		cd - > /dev/null
+	else
+		echo "$3 is not a directory!"
+	fi
+}
+function print_suffix_usage {
+
+	echo  'Usage:'
+	echo  'suffix <original> <new> <file/directory>'
 }
