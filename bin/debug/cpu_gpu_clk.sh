20220904463adefb79a6e7a9a7c45e06a716c431
gyl <gyl33333@gmail.com>
2016-07-04 17:40:02 +0800

    [vim][bin]Modify the node of cpu freq
    Modify the node of cpu freq

--- bin/debug/cpu_gpu_clk.sh
+++ bin/debug/cpu_gpu_clk.sh
@@ -3,7 +3,7 @@ clk() {
 	do
 		adb wait-for-device
 		echo "****************cpu clk***************"
-		adb shell "cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq"
+		adb shell "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq"
 		echo "****************gpu clk***************"
 		adb shell "cat /sys/class/kgsl/kgsl-3d0/gpuclk"
 		echo "****************gpubusy***************"
