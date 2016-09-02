2aafb5f536cba3f9d18840ae21914df71f2950e7
gyl <gyl33333@gmail.com>
2016-09-02 17:38:29 +0800

    [vim][gyl]Add the errorhanldling of adb-connect
    Add the errorhanldling of adb-connect

--- bin/debug/adb-connect.sh
+++ bin/debug/adb-connect.sh
@@ -7,6 +7,12 @@ adb-connect() {
 	adb shell setprop service.adb.tcp.port 5555
 
 	ip=`adb shell ifconfig wlan0|grep 'inet addr'|awk -F'[ : ]' '{print $13}'`
+
+	if [ -z $ip ]; then
+		echo -e "\nThe device does not connect to any AP\n"
+		return
+	fi
+
 	echo "********************************"
 	echo "Run Command:                   *"
 	echo "adb connect $ip:5555 *"
