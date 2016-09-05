c40d48c1874e9d29496d24abef112ad0d1b46652
gyl <gyl33333@gmail.com>
2016-09-05 17:23:05 +0800

    [vim][gyl]Add the mac support of grep
    Add the mac support of grep

--- bin/tools/gg.sh
+++ bin/tools/gg.sh
@@ -1,7 +1,12 @@
 
 gg () {
 	value="$@"
-	~/.vim/bin/tools/grep --color=auto -inrH "$value" ./
+	sysos=`uname -s`
+	if [ $sysos == "Linux" ]; then
+		~/.vim/bin/tools/grep_linux --color=auto -inrH "$value" ./
+	elif [ $sysos == "Darwin" ]; then
+		~/.vim/bin/tools/grep_mac --color=auto -inrH "$value" ./
+	fi
 	value=""
 }
 
