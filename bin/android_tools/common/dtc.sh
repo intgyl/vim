157d736f6945187cbdf5ad7d15297d1c1521d1c4
gyl <gyl33333@gmail.com>
2016-09-30 11:40:42 +0800

    dtc:modify the stuff error
    modify the stuff error

--- bin/android_tools/common/dtc.sh
+++ bin/android_tools/common/dtc.sh
@@ -5,16 +5,16 @@ dtc() {
 		return
 	fi
 
-	if [ -z `echo $1 | awk -F "." '{print $2}'` ]; then
+	if [ -z `echo $1 | sed 's/^.*\.//'` ]; then
 		print_usage
 		return
 
-	elif [ `echo $1 | awk -F "." '{print $2}'` != "dtb" ]; then
+	elif [ `echo $1 | sed 's/^.*\.//'` != "dtb" ]; then
 		print_usage
 		return
 	fi
 
-	$HOME/.vim/bin/android_tools/common/dtc_bin -I dtb -O dts -o $(echo $1 | awk -F "." '{print$1}').dts $1
+	$HOME/.vim/bin/android_tools/common/dtc_bin -I dtb -O dts -o $(echo $1 | sed 's/\.[^.]*$//').dts $1
 }
 
 function print_usage {
