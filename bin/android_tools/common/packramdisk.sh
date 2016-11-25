0e6bc30340ebefab5b7c358b455d6dfdb4334c29
gyl <gyl33333@gmail.com>
2016-11-25 17:47:53 +0800

    android_tools: modify the permission
    modify the permission

--- bin/android_tools/common/packramdisk.sh
+++ bin/android_tools/common/packramdisk.sh
@@ -15,34 +15,36 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-version="packramdisk 1.0
-Written by Kidd Lu."
+packramdisk() {
+	version="packramdisk 1.0
+	Written by Kidd Lu."
 
-usage="Usage: packramdisk dir [output file]... ...
+	usage="Usage: packramdisk dir [output file]... ...
 
-if no output file, use default ./ramdisk.img as output file"
+	if no output file, use default ./ramdisk.img as output file"
 
-case $1 in
---help)    exec echo "$usage";;
---version) exec echo "$version";;
-esac
+		case $1 in
+			--help)    exec echo "$usage";;
+		--version) exec echo "$version";;
+	esac
 
-INPUT=$1
+	INPUT=$1
 
-if [ ! -d "$INPUT" ]; then 
-  echo -e "\033[31mErr: $INPUT does not exist\033[0m"
-  echo ""
-  exec echo "$usage"
-  exit 1 
-fi
+	if [ ! -d "$INPUT" ]; then 
+		echo -e "\033[31mErr: $INPUT does not exist\033[0m"
+		echo ""
+		exec echo "$usage"
+		exit 1 
+	fi
 
-if  [ ! -n "$2" ] ;then
-  OUTPUT=`pwd`/ramdisk.img
-else
-  OUTPUT=`readlink -f $2`
-fi
+	if  [ ! -n "$2" ] ;then
+		OUTPUT=`pwd`/ramdisk.img
+	else
+		OUTPUT=`readlink -f $2`
+	fi
 
-echo "pack ramdisk to $OUTPUT"
+	echo "pack ramdisk to $OUTPUT"
 
-exec mkbootfs "$INPUT" | minigzip > $OUTPUT
-#cd `readlink -f $INPUT`;find . | cpio -o -H newc | gzip > $OUTPUT
+	exec mkbootfs "$INPUT" | minigzip > $OUTPUT
+	#cd `readlink -f $INPUT`;find . | cpio -o -H newc | gzip > $OUTPUT
+}
--- bin/android_tools/common/unpackramdisk.sh
+++ bin/android_tools/common/unpackramdisk.sh
@@ -15,37 +15,39 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-version="unpackramdisk 1.0
-Written by Kidd Lu."
+unpackramdisk() {
+	version="unpackramdisk 1.0
+	Written by Kidd Lu."
 
-usage="Usage: unpackramdisk file [output dir]... ...
+	usage="Usage: unpackramdisk file [output dir]... ...
 
-if no output dir, use default ./ramdisk as output dir"
+	if no output dir, use default ./ramdisk as output dir"
 
-case $1 in
---help)    exec echo "$usage";;
---version) exec echo "$version";;
-esac
+		case $1 in
+			--help)    exec echo "$usage";;
+		--version) exec echo "$version";;
+	esac
 
-INPUT_FILE=`readlink -f $1`
+	INPUT_FILE=`readlink -f $1`
 
-if [ ! -f "$INPUT_FILE" ]; then 
-  echo -e "\033[31mErr: $1 does not exist\033[0m"
-  echo ""
-  exec echo "$usage"
-  exit 1 
-fi
+	if [ ! -f "$INPUT_FILE" ]; then 
+		echo -e "\033[31mErr: $1 does not exist\033[0m"
+		echo ""
+		exec echo "$usage"
+		exit 1 
+	fi
 
-if  [ ! -n "$2" ] ;then
-  OUTPUT_DIR=./ramdisk
-else
-  OUTPUT_DIR=$2
-fi
+	if  [ ! -n "$2" ] ;then
+		OUTPUT_DIR=./ramdisk
+	else
+		OUTPUT_DIR=$2
+	fi
 
-if [ ! -d "$OUTPUT_DIR" ]; then
-  mkdir $OUTPUT_DIR
-fi
-echo "output to dir $OUTPUT_DIR"
+	if [ ! -d "$OUTPUT_DIR" ]; then
+		mkdir $OUTPUT_DIR
+	fi
+	echo "output to dir $OUTPUT_DIR"
 
-cd $OUTPUT_DIR
-exec gzip -d -c "$INPUT_FILE" | cpio -idu
+	cd $OUTPUT_DIR
+	exec gzip -d -c "$INPUT_FILE" | cpio -idu
+}
