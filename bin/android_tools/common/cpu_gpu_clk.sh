2dad806dc3b10a9e0be2309fc40561cb4073ec0c
gyl <gyl33333@gmail.com>
2017-01-09 14:45:26 +0800

    clk: add cpuclk gpuclk
    add cpuclk gpuclk

--- bin/android_tools/common/cpu_gpu_clk.sh
+++ bin/android_tools/common/cpu_gpu_clk.sh
@@ -12,3 +12,22 @@ clk() {
 		sleep 0.5
 	done
 }
+
+cpuclk() {
+	while true
+	do
+		adb wait-for-device
+		adb shell "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq"
+		sleep 0.5
+	done
+
+}
+
+gpuclk() {
+	while true
+	do
+		adb wait-for-device
+		adb shell "cat /sys/class/kgsl/kgsl-3d0/gpuclk"
+		sleep 0.5
+	done
+}
