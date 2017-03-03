62a525ec2ae4c42d6ae486fa4e0d06a19fd25c68
guanyanliang <guanyanliang@smartisan.com>
2017-03-03 19:28:00 +0800

    chrome: chrome download for mac
    chrome download for mac

--- bin/install.sh
+++ bin/install.sh
@@ -126,6 +126,7 @@ sudo $apt install wiznote
 
 #chrome
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 
 #wine
 #sudo add-apt-repository ppa:ubuntu-wine/ppa -y
