7e302f39b357920c073885d5b92f040e7af58c78
gyl <gyl33333@gmail.com>
2017-05-02 17:48:34 +0800

    killpid: add support for mac
    add support for mac

--- bin/tools/common/killpid.sh
+++ bin/tools/common/killpid.sh
@@ -1,3 +1,8 @@
 kp() {
-	ps -u `whoami` | grep $1| awk '{print$1}' | xargs kill -9
+	sysos=`uname -s`
+	if [ $sysos == "Linux" ]; then
+		ps -u `whoami` | grep $1| awk '{print$1}' | xargs kill -9
+	elif [ $sysos == "Darwin" ]; then
+		ps -u `whoami` | grep $1| awk '{print$2}' | xargs kill -9
+	fi
 }
