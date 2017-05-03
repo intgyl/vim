2c7e93a9d83e58a21fa445a15dae886c72522cfb
gyl <gyl33333@gmail.com>
2017-05-03 09:59:26 +0800

    tounix: add support for single file
    add support for single file

--- bin/tools/common/tounix.sh
+++ bin/tools/common/tounix.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 tounix () {
-    for file in `ls $1`
-    do
-        if [ -d $1"/"$file ]
-        then
-            tounix $1"/"$file
-        else
-            dos2unix $1"/"$file
-            #echo $1"/"$file
-        fi
-    done
+	if [ -d $1 ]; then
+
+		for file in `ls $1`
+		do
+			if [ -d $1"/"$file ]
+			then
+				tounix $1"/"$file
+			else
+				dos2unix $1"/"$file
+				#echo $1"/"$file
+			fi
+		done
+	else
+		dos2unix $1
+	fi
 }
