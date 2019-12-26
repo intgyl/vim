006d44830a4561a9f0e51af3afad5c94a1245326
gyl <intgyl@gmail.com>
2019-12-26 15:04:33 +0800

    astyle: modify astyle config
    modify astyle config

--- bin/tools/common/ast.sh
+++ bin/tools/common/ast.sh
@@ -7,37 +7,37 @@ ast () {
 		if [ -d $value ]; then
 			if [ $(find $value -type d | wc -l) -gt 1 ]; then
 
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.cc
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.cpp
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.h
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.c
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.cc
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.cpp
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.h
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.c
 			else
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.cc
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.cpp
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.h
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.c
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.cc
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.cpp
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.h
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.c
 
 			fi
 		else
-			$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value
+			$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value
 		fi
 	elif [ $sysos == "Darwin" ]; then
 		if [ -d $value ]; then
 			if [ $(find $value -type d | wc -l) -gt 1 ]; then
 
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.cc
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.cpp
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.h
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none --recursive $value/*.c
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.cc
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.cpp
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.h
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive $value/*.c
 			else
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.cc
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.cpp
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.h
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value/*.c
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.cc
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.cpp
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.h
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value/*.c
 
 			fi
 		else
-			$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --suffix=none $value
+			$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none $value
 
 		fi
 	fi
