0e5b4605389e7078f6b1567c69d1499d9584ddae
gyl <intgyl@gmail.com>
2026-08-11 11:04:34 +0800

    update ast

--- bin/tools/common/ast.sh
+++ bin/tools/common/ast.sh
@@ -7,37 +7,55 @@ ast () {
 		if [ -d $value ]; then
 			if [ $(find $value -type d | wc -l) -gt 1 ]; then
 
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
+				clang-format -i *.c
+				clang-format -i *.h
+				clang-format -i *.cpp
+				clang-format -i *.cc
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
 			else
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
-				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
+				clang-format -i *.c
+				clang-format -i *.h
+				clang-format -i *.cpp
+				clang-format -i *.cc
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
+				$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
 
 			fi
 		else
-			$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value
+			clang-format -i $value
+			$HOME/.vim/bin/tools/linux/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value
 		fi
 	elif [ $sysos == "Darwin" ]; then
 		if [ -d $value ]; then
 			if [ $(find $value -type d | wc -l) -gt 1 ]; then
 
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
+				clang-format -i *.c
+				clang-format -i *.h
+				clang-format -i *.cpp
+				clang-format -i *.cc
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --recursive --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
 			else
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
-				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
+				clang-format -i *.c
+				clang-format -i *.h
+				clang-format -i *.cpp
+				clang-format -i *.cc
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cc
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.cpp
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.h
+				$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value/*.c
 
 			fi
 		else
-			$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all --indent-switches --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value
+			clang-format -i $value
+			$HOME/.vim/bin/tools/mac/astyle --style=linux -p --indent=force-tab=8 --break-blocks=all  --pad-oper --pad-comma --pad-header --suffix=none --align-pointer=name --align-reference=name --break-one-line-headers --attach-return-type --attach-return-type-decl $value
 
 		fi
 	fi
